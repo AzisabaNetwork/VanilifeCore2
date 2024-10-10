78e026819386d8c6e6f989340d58a7ccbd916b26
tksimeji <tksimieji@outlook.com>
2024-10-10 10:26:27 +0900

    1.8.8: Housing Change the message for visitors.

--- src/main/java/net/azisaba/vanilife/housing/HousingListener.java
+++ src/main/java/net/azisaba/vanilife/housing/HousingListener.java
@@ -57,7 +57,7 @@ public class HousingListener implements Listener
 
         if (! can)
         {
-            if (HousingPacks.registry().stream().anyMatch(pack -> pack.include(event.getBlock())))
+            if (housing.getUser() == user && HousingPacks.registry().stream().anyMatch(pack -> pack.include(event.getBlock())))
             {
                 IHousingPack pack = HousingPacks.registry().stream().filter(p -> p.include(event.getBlock())).toList().getFirst();
                 player.sendMessage(Language.translate("housing.cant-place.how", player, "pack=" + ComponentUtility.asString(Language.translate("housing.pack." + pack.getName() + ".name", player))));
